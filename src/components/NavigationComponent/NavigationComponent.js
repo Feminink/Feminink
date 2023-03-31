5517c0780c1ffdc51c5ec36ace0be37cfc8e9ab7
laurellx <laura.origa@gmail.com>
2023-03-31 22:44:20 +0200

    protected routes ok

--- src/components/NavigationComponent/NavigationComponent.js
+++ src/components/NavigationComponent/NavigationComponent.js
@@ -1,10 +1,20 @@
-// IMPORT LINK
+// IMPORT LINK,
 import { Link } from "react-router-dom";
+// IMPORT USE SELECTOR
+import { useDispatch, useSelector } from "react-redux";
+// IMPORT API CALL DO LOGOUT
+import { doLogout } from "../../store/auth/actions";
 import React from "react";
 import PropTypes from "prop-types";
 import "./NavigationComponent.scss";
 
 const NavigationComponent = () => {
+  const { user } = useSelector((state) => state.AuthReducer);
+  const dispatch = useDispatch();
+
+  function onClickLogout() {
+    dispatch(doLogout());
+  }
   return (
     <nav className="nav">
       <ul className="nav-ul">
@@ -17,12 +27,29 @@ const NavigationComponent = () => {
         <li className="nav-li">
           <Link to="/about">About</Link>
         </li>
-        <li className="nav-li">
-          <Link to="/login">Login</Link>
-        </li>
-        <li className="nav-li">
-          <Link to="/profile">Profile</Link>
-        </li>
+        {user && user.id ? (
+          ""
+        ) : (
+          <li className="nav-li">
+            <Link to="/login">Login</Link>
+          </li>
+        )}
+        {user && user.id ? (
+          <li className="nav-li">
+            <Link to="/profile">Profile</Link>
+          </li>
+        ) : (
+          ""
+        )}
+        {user && user.id ? (
+          <li className="nav-li">
+            <Link to="/" onClick={onClickLogout}>
+              Logout
+            </Link>
+          </li>
+        ) : (
+          ""
+        )}
       </ul>
     </nav>
   );
