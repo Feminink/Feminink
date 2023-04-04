8576a9b52c42b46c2891c0184880a3ee573349ee
Miriam Fraga Rodríguez <fraga@MacBook-Pro-de-Miriam.local>
2023-04-04 10:45:37 +0200

    added info about tattoo artists

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-
+import circus from '../../assets/images/circus.png'
 const Home = () => {
-  return <div></div>;
+  return <div>
+    <img src={circus} style={{width: "150px"}} alt={circus} className="img__logo"></img>
+  </div>;
 };
 
 export default Home;
