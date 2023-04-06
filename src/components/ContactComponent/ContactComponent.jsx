311d5560cdc9f9d70f2ed1e6ee351b4c33b138de
Miriam Fraga Rodríguez <fraga@MacBook-Pro-de-Miriam.local>
2023-04-06 20:56:45 +0200

    Preparation to formik form

--- src/components/ContactComponent/ContactComponent.jsx
+++ src/components/ContactComponent/ContactComponent.jsx
@@ -6,8 +6,11 @@ import { getArtists } from '../../store/Tattoo/actions';
 
 // IMPORT FUNCIÖN DE CONTACTAR 
 import { doContact } from '../../store/Tattoo/actions';
+
 //IMPORT HOOKS
 import { useDispatch, useSelector } from 'react-redux';
+
+//IMPORT HOOK FORMIK
 
 
 const ContactComponent = () => { 
@@ -15,25 +18,61 @@ const ContactComponent = () => {
 const dispatch = useDispatch();
 
 const {artists, loadingArtists} = useSelector((state)=>state.TattooReducer);
-// const {form} = useSelector((state)=>state.TattooReducer);
+//  const {form} = useSelector((state)=>state.TattooReducer);
 
-//CREO ESTADOS PARA SETEAR VALORES DE LOS INPUTS
-const [name, setName] = useState("");
-const [description, setDescription] = useState("");
-const [email, setEmail] = useState("");
-const [color, setColor] = useState("#000000");
-const [artist, setArtist] = useState("");
+// USAR SETSTATE PARA RECOGER TODOS LOS NUEVOS VALORES DE LOS ESTADOS CON ESTA FUNCIÓN
 
-// COMPILAR TODOS LOS ESTADOS DENTRO DE UNO Y USAR SETSTATE
-// const [userForm, setUserForm] = useState({ name: "", email: "", color: "", artists: "artist", description: ""});
+ const handleChange  = (e) =>{
+  const {name, value} = e.target;
+  setUserDataForm({...userDataForm, [name]: value});
+ };
 
 useEffect(()=>{
   dispatch(getArtists())
 },[])
 
 const sendForm = () =>{
-  dispatch(doContact({name: name, description: description, email: email, color: color, artist: artist}))
+  dispatch(doContact({userDataForm}))
 }
+
+//AUNAR TODOS LOS ESTADOS EN UNO SOLO
+const [userDataForm, setUserDataForm] = useState({ name: "", email: "", color: "", description: "", artist: ""});
+
+//VARIABLE QUE ALMACENA LOS MENSAJES SI SE PRODUCEN ERRORES
+// const validation = (values) => {
+  // const errors = {};
+
+  // if (!values.email) {
+    // errors.email = "Required";
+  // } else if (values.email.length < 4) {
+    // errors.email = "Must be 5 characters or more";
+  // }
+
+  // if (!values.password) {
+    // errors.password = "Required";
+  // } else if (values.password.length < 8) {
+    // errors.password = "Must be 8 characters or more";
+  // } else if (values.password === "12345678") {
+    // errors.password = "Must not be 12345678 !!!";
+  // }
+
+  // if (!values.repassword) {
+    // errors.repassword = "Required";
+  // } else if (values.repassword !== values.password) {
+    // errors.repassword = "Second password doesn't match";
+  // }
+
+  // return errors;
+// };
+
+
+// const messageError = {
+  // req: "Campo obligatorio",
+  // name: "Este campo no admite caracteres especiales, ni numéricos",
+  // email: "Debes introducir una dirección correcta",
+  // description: "Longitud máxima 70 caracteres"
+// }
+
 
 if (loadingArtists){
   return (
@@ -44,33 +83,33 @@ if (loadingArtists){
           <form className='form'> 
             <fieldset className="form__container" >
               <label>Nombre </label>
-              <input type="text" defaultValue={name} onChange={(e)=>setName(e.target.value)}  ></input>
+              <input name="name" type="text" defaultValue={userDataForm.name} onChange={handleChange} placeholder= "Escribe tu nombre" ></input>
             </fieldset>
 
             <fieldset className="form__container">
               <label>Email </label>
-              <input type="email" defaultValue={email} onChange={(e)=>setEmail(e.target.value)} required></input>
+              <input name="email" type="email" defaultValue={userDataForm.email} onChange={handleChange} placeholder= "Email de contacto" required></input>
               </fieldset>
   
             <fieldset className="form__container">
                <label>Descripción del tatuaje </label>
-               <textarea type="text" defaultValue={description} onChange={(e)=>setDescription(e.target.value)} required></textarea>
+               <textarea  name="description" type="text" defaultValue={userDataForm.description} onChange={handleChange} placeholder= "Breve descripción del diseño" required></textarea>
             </fieldset>
 
             <fieldset className="form__container">
                   <label>Artista</label>
-                     <select value={artist} onChange={(e) => setArtist(e.target.value)} required>
+                     <select name="artist" value={userDataForm.artist.name} onChange={handleChange} required>
                         {artists?.map((artist) => {
                             return (
                               
-                              <option key={artist.id} value={artist.name}> {artist.name} </option>);
+                              <option name="artist" key={artist.id} defaultValue={userDataForm.artist.name}> {artist.name} </option>);
                           })}
                   </select>
             </fieldset>
 
             <fieldset className="form__container">
                <label>Color base </label>
-               <input type="color" defaultValue={color} onChange={(e)=>setColor(e.target.value)} required></input>
+               <input name="color" type="color" defaultValue={userDataForm} onChange={handleChange} required></input>
             </fieldset>
 
           </form>
