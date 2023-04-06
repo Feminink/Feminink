67412c7a8e5d43c40b2da90bf1078cbecee6463f
Miriam Fraga Rodríguez <fraga@MacBook-Pro-de-Miriam.local>
2023-04-06 23:07:32 +0200

    not ok form with validators

--- src/components/ContactComponent/ContactComponent.jsx
+++ src/components/ContactComponent/ContactComponent.jsx
@@ -14,110 +14,110 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useFormik } from 'formik';
 
 const ContactComponent = () => { 
-
+  useEffect(()=>{
+    dispatch(getArtists())
+  },[])
+  
+  const sendForm = () =>{
+    dispatch(doContact())
+  }
+  
 const dispatch = useDispatch();
 
-const {artists, loadingArtists} = useSelector((state)=>state.TattooReducer);
-//  const {form} = useSelector((state)=>state.TattooReducer);
+// const {artists, loadingArtists} = useSelector((state)=>state.TattooReducer);
 
 // USAR SETSTATE PARA RECOGER TODOS LOS NUEVOS VALORES DE LOS ESTADOS CON ESTA FUNCIÓN
 
- const handleChange  = (e) =>{
-  const {name, value} = e.target;
-  setUserDataForm({...userDataForm, [name]: value});
- };
+//  const handleChange  = (e) =>{
+  // const {name, value} = e.target;
+  // setUserDataForm({...userDataForm, [name]: value});
+//  };
 
-useEffect(()=>{
-  dispatch(getArtists())
-},[])
+function validate (values) {
+  const errors = {};
 
-const sendForm = () =>{
-  dispatch(doContact({userDataForm}))
-}
+  if (!values.email) {
+    errors.email = "Required";
+  } else if (values.email.length < 4) {
+    errors.email = "Must be 5 characters or more";
+  }
+
+  if (!values.name) {
+    errors.password = "Required";
+  } else if (values.password.length < 8) {
+    errors.password = "Must be 8 characters or more";
+  } else if (values.password === "12345678") {
+    errors.password = "Must not be 12345678 !!!";
+  }
+
+  if (!values.description) {
+    errors.repassword = "Required";
+  } else if (values.repassword.length < 50) {
+    errors.repassword = "Máximo 50 caracteres";
+  }
+
+  return errors;
+}};
+
+
+const formik = useFormik({ initial: {name: "", email: "", color: "", description: "", artist: ""},
+              onSubmit: (values)=>{
+                alert(JSON.stringify(values, null, 2));
+              }
+});
 
 //AUNAR TODOS LOS ESTADOS EN UNO SOLO
-const [userDataForm, setUserDataForm] = useState({ name: "", email: "", color: "", description: "", artist: ""});
+// const [userDataForm, setUserDataForm] = useState({ name: "", email: "", color: "", description: "", artist: ""});
 
 //VARIABLE QUE ALMACENA LOS MENSAJES SI SE PRODUCEN ERRORES
-// const validation = (values) => {
-  // const errors = {};
 
-  // if (!values.email) {
-    // errors.email = "Required";
-  // } else if (values.email.length < 4) {
-    // errors.email = "Must be 5 characters or more";
-  // }
-
-  // if (!values.password) {
-    // errors.password = "Required";
-  // } else if (values.password.length < 8) {
-    // errors.password = "Must be 8 characters or more";
-  // } else if (values.password === "12345678") {
-    // errors.password = "Must not be 12345678 !!!";
-  // }
-
-  // if (!values.repassword) {
-    // errors.repassword = "Required";
-  // } else if (values.repassword !== values.password) {
-    // errors.repassword = "Second password doesn't match";
-  // }
-
-  // return errors;
-// };
-
-
-// const messageError = {
-  // req: "Campo obligatorio",
-  // name: "Este campo no admite caracteres especiales, ni numéricos",
-  // email: "Debes introducir una dirección correcta",
-  // description: "Longitud máxima 70 caracteres"
-// }
-
-
-if (loadingArtists){
+if (loadingInfo){
   return (
     <p> "Loading"</p>
   )
 }else{
        return(<section className='section__login section'>
-          <form className='form'> 
+          <form onSubmit={formik.handleSubmit} className='form'> 
           <h1>Formulario de contacto: </h1>
             <fieldset className="form__container" >
               <label>Nombre </label>
-              <input name="name" type="text" defaultValue={userDataForm.name} onChange={handleChange} placeholder= "Escribe tu nombre" ></input>
+              <input name="name" type="text" value={formik.values.name} onChange={formik.handleChange} placeholder= "Escribe tu nombre" required></input>
             </fieldset>
 
             <fieldset className="form__container">
               <label>Email </label>
-              <input name="email" type="email" defaultValue={userDataForm.email} onChange={handleChange} placeholder= "Email de contacto" required={}></input>
+              <input name="email" type="email" value={formik.values.email} onChange={formik.handleChange} placeholder= "Email de contacto" required></input>
+              {formik.touched.email && formik.errors.email ? (
+                    <div className="error">{formik.errors.email}</div>
+                  ) : null}
               </fieldset>
   
             <fieldset className="form__container">
                <label>Descripción del tatuaje </label>
-               <textarea  name="description" type="text" defaultValue={userDataForm.description} onChange={handleChange} placeholder= "Breve descripción del diseño" required></textarea>
+               <textarea  name="description" type="text" value={formik.values.description} onChange={formik.handleChange} placeholder= "Breve descripción del diseño" required></textarea>
             </fieldset>
 
             <fieldset className="form__container">
                   <label>Artista</label>
-                     <select name="artist" value={userDataForm.artist.name} onChange={handleChange} required>
+                     <select name="artist" value={formik.values.artist} onChange={formik.handleChange} required>
                         {artists?.map((artist) => {
                             return (
                               
-                              <option name="artist" key={artist.id} defaultValue={userDataForm.artist.name}> {artist.name} </option>);
+                              <option name="artist" key={artist.id} value={formik.values.artist}> {artist.name} </option>);
                           })}
                   </select>
             </fieldset>
 
             <fieldset className="form__container">
                <label>Color base </label>
-               <input name="color" type="color" defaultValue={userDataForm} onChange={handleChange} required></input>
+               <input name="color" type="color" value={formik.values.color} onChange={formik.handleChange} required></input>
             </fieldset>
 
           </form>
-          <button type="button" onClick={sendForm}> Enviar </button>
+          <button type="submit" onClick={sendForm}> Enviar </button>
        </section>)
 
-}  }
+}  
 ContactComponent.propTypes = {};
 
 ContactComponent.defaultProps = {};
