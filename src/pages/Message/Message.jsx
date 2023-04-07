17e6792ad47a494d0e27a194278cc2e86c70a58b
Miriam Fraga Rodríguez <fraga@MacBook-Pro-de-Miriam.local>
2023-04-07 20:25:18 +0200

    messages

--- src/pages/Message/Message.jsx
+++ src/pages/Message/Message.jsx
@@ -1,35 +1,23 @@
 import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch} from 'react-redux';
 import { useParams } from 'react-router-dom'
+import SingleMessage from '../../components/SingleMessage/SingleMessage';
 import { getSingleMessage } from '../../store/Tattoo/actions';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPalette} from '@fortawesome/free-solid-svg-icons';
 
 const Message = () => {
+  const dispatch = useDispatch();
 
     const params = useParams();
-    const dispatch = useDispatch()
-    const {message}= useSelector((state)=>state.TattooReducer)
-    console.log(message, "message")
-
     useEffect(()=>{
-        dispatch(getSingleMessage(params.id))
+      dispatch(getSingleMessage(params.id))
     },[])
+ 
   return (
     <div>
-    
-           <div key={message.id} className='section__messages__div'> 
-         
-         <h2>De: {message.name}</h2>
-         <h3>Para: {message.artist}</h3>
-         <h3 className="" > {message.email}</h3>
-
-         <h3 className="" > {message.description}</h3>
-         <h3  className="" style={{color: message.color}}> <FontAwesomeIcon icon={faPalette} /> {message.color} </h3>
-        
+    <SingleMessage></SingleMessage>
          </div>
 
-    </div>
+   
   )
 }
 
--- src/pages/Messages/Messages.jsx
+++ src/pages/Messages/Messages.jsx
@@ -1,17 +1,25 @@
 import React, { useEffect, useState } from 'react'
+
+//IMPORT HOOKS
 import { useDispatch, useSelector } from 'react-redux'
+
+//IMPORT LINK
 import { Link } from 'react-router-dom'
+
+//IMPORTO FUNCIÓN PARA HACER EL GET
 import {getMessages} from '../../store/Tattoo/actions'
+
+//IMPORT SCSS
 import  './Messages.scss'
 
+//IMPORT FONTAWESOME
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPalette} from '@fortawesome/free-solid-svg-icons';
+import { faPalette, faMessage} from '@fortawesome/free-solid-svg-icons';
+
 const Messages = () => {
 
 const dispatch = useDispatch();
-
 const {messages, loadingMessages} = useSelector((state)=> state.TattooReducer);
-
 const [isShown, setIsShown] = useState("false");
 
 const handleToggle = (e) =>{
@@ -34,22 +42,25 @@ if (loadingMessages){
   }
   return (
     <section className='section__messages'>
-     <h3 className='counter'>Bandeja de entrada: {messages.length}</h3>
+     <h3 className='counter'> Bandeja de entrada:  {messages.length} <FontAwesomeIcon icon={faMessage} /> </h3>
     {messages.map((message)=>{
         return (
+       <div className='wrapper'> 
+              <div key={message.id} className='section__messages__div'> 
+                   <h2>De: {message.name}</h2>
+                   <h3>Para: {message.artist}</h3>
+                   <h3  className="h3__hidden" > {message.email}</h3>
+                   <h3  className="h3__hidden" > {message.description}</h3>
+                   <h3  className="h3__hidden" style={{color: message.color}}> <FontAwesomeIcon icon={faPalette} /> {message.color} </h3>
+                   <Link className='link' to={`/message/${message.id}`}><h3> Leer</h3> </Link> 
           
-       <div key={message.id} className='section__messages__div'> 
-           
-            <h2>De: {message.name}</h2>
-            <h3>Para: {message.artist}</h3>
-            <h3 className={`h3__hidden${isShown?  "" : "hidden"}`} > {message.email}</h3>
-            <h3 className={`h3__hidden${isShown?  "" : "hidden"}`} > {message.description}</h3>
-            <h3  className={`h3__hidden${isShown?  "" : "hidden"}`} style={{color: message.color}}> <FontAwesomeIcon icon={faPalette} /> {message.color} </h3>
-            <Link to={`/message/${message.id}`}> <button className="button" onClick={handleToggle} >Leer</button></Link> 
-            </div>
-           
+              </div>
+        </div>
         )
     })}
+    
+    
+    
 
     </section>
   )
