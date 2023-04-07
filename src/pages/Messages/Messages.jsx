0c11bcb93a9e953c808b31627fbb4962212d2251
Miriam Fraga Rodríguez <fraga@MacBook-Pro-de-Miriam.local>
2023-04-07 13:50:37 +0200

    save little changes

--- src/pages/Messages/Messages.jsx
+++ src/pages/Messages/Messages.jsx
@@ -40,6 +40,7 @@ if (loadingMessages){
             <div className='section__messages__div'> 
            
             <h2>De: {message.name}</h2>
+            <h3>Para: {message.artist}</h3>
             <h3 className={`h3__hidden${isShown?  "" : "hidden"}`} > {message.email}</h3>
             <h3 className={`h3__hidden${isShown?  "" : "hidden"}`} > {message.description}</h3>
             <h3  className={`h3__hidden${isShown?  "" : "hidden"}`} style={{color: message.color}}> <FontAwesomeIcon icon={faPalette} /> {message.color} </h3>
