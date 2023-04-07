a986e7dc994a97691acd20b6f3ea03e799e09217
Miriam Fraga Rodríguez <fraga@MacBook-Pro-de-Miriam.local>
2023-04-07 13:09:38 +0200

    merge validate

--- src/components/ContactComponent/ContactComponent.jsx
+++ src/components/ContactComponent/ContactComponent.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect} from 'react';
 import PropTypes from 'prop-types';
 import  './ContactComponent.scss';
 
-import { getArtists } from '../../store/Tattoo/actions';
+import {getInfo} from '../../store/info/actions'
 
 // IMPORT FUNCIÖN DE CONTACTAR 
 import { doContact } from '../../store/Tattoo/actions';
@@ -16,13 +16,13 @@ import { useFormik } from 'formik';
 const ContactComponent = () => { 
 
   useEffect(()=>{
-    dispatch(getArtists())
+    dispatch(getInfo())
   },[])
   
   const sendForm = () =>{
     dispatch(doContact())
   }
-  
+  const {info, loadinginfo} = useSelector((state)=> state.InfoReducer)
 const dispatch = useDispatch();
 
   function validate (values) {
@@ -50,66 +50,69 @@ const dispatch = useDispatch();
     return errors;
     
   }
+
+  const formik = useFormik({
+    initial: {
+     name: "",
+     email: "",
+     color: "",
+     description: "",
+     artist: "",
+   },validate,
+ 
+     onSubmit: (values)=>{
+                 alert(JSON.stringify(values, null, 2));
+               },
+             });
+ if(loadinginfo){
+  return(
+    <p>"Loading..."</p>
+  )
+ }
+        return(
+        <section className='section__login section'>
+           <form onSubmit={formik.handleSubmit} className='form'> 
+           <h1>Formulario de contacto: </h1>
+             <fieldset className="form__container" >
+               <label>Nombre </label>
+               <input name="name" type="text" value={formik.values.name} onChange={formik.handleChange} placeholder= "Escribe tu nombre" required></input>
+             </fieldset>
+ 
+             <fieldset className="form__container">
+               <label>Email </label>
+               <input name="email" type="email" value={formik.values.email} onChange={formik.handleChange} placeholder= "Email de contacto" required></input>
+               {formik.touched.email && formik.errors.email ? (
+                     <div className="error">{formik.errors.email}</div>
+                   ) : null}
+               </fieldset>
+   
+             <fieldset className="form__container">
+                <label>Descripción del tatuaje </label>
+                <textarea  name="description" type="text" value={formik.values.description} onChange={formik.handleChange} placeholder= "Breve descripción del diseño" required></textarea>
+             </fieldset>
+ 
+             <fieldset className="form__container">
+                   <label>Artista</label>
+                      <select name="artist" value={formik.values.artist} onChange={formik.handleChange} required>
+                         {info && info.artists?.map((artist) => {
+                             return (
+                               
+                               <option name="artist" key={artist.id} value={formik.values.artist}> {artist.name} </option>);
+                           })}
+                   </select>
+             </fieldset>
+ 
+             <fieldset className="form__container">
+                <label>Color base </label>
+                <input name="color" type="color" value={formik.values.color} onChange={formik.handleChange} required></input>
+             </fieldset>
+ 
+           </form>
+           <button type="submit" onClick={sendForm}> Enviar </button>
+        </section>)
+ 
   
 };
-
-
-const formik = useFormik({
-   initial: {
-    name: "",
-    email: "",
-    color: "",
-    description: "",
-    artist: "",
-  },
-
-    onSubmit: (values)=>{
-                alert(JSON.stringify(values, null, 2));
-              },
-            });
-
-       return(
-       <section className='section__login section'>
-          <form onSubmit={formik.handleSubmit} className='form'> 
-          <h1>Formulario de contacto: </h1>
-            <fieldset className="form__container" >
-              <label>Nombre </label>
-              <input name="name" type="text" value={formik.values.name} onChange={formik.handleChange} placeholder= "Escribe tu nombre" required></input>
-            </fieldset>
-
-            <fieldset className="form__container">
-              <label>Email </label>
-              <input name="email" type="email" value={formik.values.email} onChange={formik.handleChange} placeholder= "Email de contacto" required></input>
-              {formik.touched.email && formik.errors.email ? (
-                    <div className="error">{formik.errors.email}</div>
-                  ) : null}
-              </fieldset>
-  
-            <fieldset className="form__container">
-               <label>Descripción del tatuaje </label>
-               <textarea  name="description" type="text" value={formik.values.description} onChange={formik.handleChange} placeholder= "Breve descripción del diseño" required></textarea>
-            </fieldset>
-
-            <fieldset className="form__container">
-                  <label>Artista</label>
-                     <select name="artist" value={formik.values.artist} onChange={formik.handleChange} required>
-                        {artists?.map((artist) => {
-                            return (
-                              
-                              <option name="artist" key={artist.id} value={formik.values.artist}> {artist.name} </option>);
-                          })}
-                  </select>
-            </fieldset>
-
-            <fieldset className="form__container">
-               <label>Color base </label>
-               <input name="color" type="color" value={formik.values.color} onChange={formik.handleChange} required></input>
-            </fieldset>
-
-          </form>
-          <button type="submit" onClick={sendForm}> Enviar </button>
-       </section>)
-
 
 ContactComponent.propTypes = {};
 
