88a7ae46c0ad752fd05395d8b38bb0815a0444a4
Miriam Fraga Rodríguez <fraga@MacBook-Pro-de-Miriam.local>
2023-04-13 01:57:06 +0200

    fixed

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Inspiration from "../Inspiration/Inspiration";
+import video from '../../assets/images/video.mp4'
 
 // IMPORT COMPONENT
 const About = () => {
   return (
     <>
     <div className="div__background">
+    <video loop autoPlay width="100%">
+    <source src={video} type="video/mp4" />
+    </video> 
     <Inspiration></Inspiration>
     </div>
      
