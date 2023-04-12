f4257baf15957ce25cb9b98f62d896e3b9b88a94
laurellx <laura.origa@gmail.com>
2023-04-12 20:45:43 +0200

    small correction I missed something when merging

--- src/store/gallery/reducer.jsx
+++ src/store/gallery/reducer.jsx
@@ -1,26 +1,20 @@
 import {
-<<<<<<< HEAD
-=======
   GET_GALLERY,
   GET_GALLERY_OK,
   GET_GALLERY_FAIL,
   GET_DETAIL,
   GET_DETAIL_OK,
   GET_DETAIL_FAIL,
->>>>>>> feature/gallerya
   DO_REGISTRATION,
   DO_REGISTRATION_OK,
   DO_REGISTRATION_FAIL,
 } from "./actionTypes";
 
 const initialState = {
-<<<<<<< HEAD
-=======
   gallery: [],
   loadingGallery: false,
   loadingDetail: false,
   detail: {},
->>>>>>> feature/gallerya
   form: {},
   loadingForm: false,
   error: {
@@ -30,8 +24,6 @@ const initialState = {
 
 export default function GalleryReducer(state = initialState, action) {
   switch (action.type) {
-<<<<<<< HEAD
-=======
     case GET_GALLERY:
       state = { ...state, loadingGallery: true };
       break;
@@ -67,7 +59,6 @@ export default function GalleryReducer(state = initialState, action) {
       };
       break;
     }
->>>>>>> feature/gallerya
     case DO_REGISTRATION:
       state = { ...state, loadingForm: true };
       break;
