e19e80625e79b300ae01c1c34d2372c670f46c17
laurellx <laura.origa@gmail.com>
2023-04-13 18:25:56 +0200

    edited gallery

--- src/components/GalleryComponent/GalleryComponent.jsx
+++ src/components/GalleryComponent/GalleryComponent.jsx
@@ -1,11 +1,14 @@
-import React, { useEffect, useState } from "react";
-import { getGallery } from "../../store/gallery/actions";
 import PropTypes from "prop-types";
+// IMPORT USE EFFECT, USE STATE
+import React, { useEffect, useState } from "react";
+// IMPORT GET GALLERY FUNCTION
+import { getGallery } from "../../store/gallery/actions";
+// IMPORT USE DISPATCH, USE SELECTOR
 import { useDispatch, useSelector } from "react-redux";
-
+// IMPORT LINK
+import { Link } from "react-router-dom";
 /* IMPORT STYLES */
 import "./GalleryComponent.scss";
-import { Link } from "react-router-dom";
 
 const GalleryComponent = () => {
   const dispatch = useDispatch();
@@ -14,23 +17,6 @@ const GalleryComponent = () => {
   );
   const artists = ["Miriam F", "Laura O", "Ignacio E"];
   const [searchByStyle, setSearchByStyle] = useState("");
-  const [filterByArtist, setFilterByArtist] = useState([]);
-
-  const filterHandler = (e) => {
-    if (e.target.checked) {
-      setFilterByArtist((previousSearch) => [
-        ...previousSearch,
-        e.target.value,
-      ]);
-    } else {
-      setFilterByArtist((previousSearch) => {
-        const index = previousSearch.indexOf(e.target.value);
-        if (index > -1) {
-          return previousSearch.splice(index, 1);
-        }
-      });
-    }
-  };
 
   useEffect(() => {
     dispatch(getGallery());
@@ -46,77 +32,80 @@ const GalleryComponent = () => {
   return (
     <section className="section__gallery container">
       <h2>Gallery</h2>
-      <section className="filters-container flex">
-        <form>
-          <fieldset className="form__group container">
-            <label>Search by style:</label>
-            <input
-              className="form__input"
-              type="search"
-              placeholder=""
-              onChange={(e) => setSearchByStyle(e.target.value)}
-            />
-          </fieldset>
-        </form>
-        <form>
-          <fieldset className="form__group">
-            <label>Search by artist name:</label>
-            <ul>
-              {artists.map((artist) => (
-                <li key={artist}>
-                  <label>
-                    <input
-                      id="checkbox"
-                      name={artist}
-                      type="checkbox"
-                      value={artist}
-                      onChange={filterHandler}
-                    />
-                    {artist}
-                  </label>
-                </li>
-              ))}
-            </ul>
-          </fieldset>
-        </form>
-      </section>
+      <div className="container flex bg">
+        <section className="filters-container flex">
+          <form>
+            <fieldset className="form__group container">
+              <label>
+                <b>Search by style:</b>
+              </label>
+              <input
+                className="form__input"
+                type="search"
+                placeholder=""
+                onChange={(e) => setSearchByStyle(e.target.value)}
+              />
+            </fieldset>
+          </form>
+          <form>
+            <fieldset className="form__group">
+              <label>
+                <b>Search by artist name:</b>
+              </label>
+              <ul>
+                {artists.map((artist) => (
+                  <li key={artist}>
+                    <label>
+                      <input
+                        id="checkbox"
+                        name={artist}
+                        type="checkbox"
+                        value={artist}
+                        onChange=""
+                      />
+                      {artist}
+                    </label>
+                  </li>
+                ))}
+              </ul>
+            </fieldset>
+          </form>
+        </section>
 
-      <ul className="gallery__ul ul">
-        {gallery
-          .filter((gallerya) => {
-            return searchByStyle.toLowerCase() === ""
-              ? gallery
-              : gallerya.style.toLowerCase().startsWith(searchByStyle);
-          })
-          .filter((gallerya) => {
-            return filterByArtist;
-          })
-          .map((gallerya) => {
-            return (
-              <li className="gallery__li li" key={gallerya.id}>
-                <div className="image-container">
-                  <Link to={`/gallery/${gallerya.id}`}>
-                    <img src={gallerya.image} alt={gallerya.alt} />
-                    <div className="overlay">
-                      <div className="text">
-                        <h3>{gallerya.title}</h3>
-                        <p>
-                          <b>Artist:</b> {gallerya.artist}
-                        </p>
-                        <p>
-                          <b>Style:</b> {gallerya.style}
-                        </p>
+        <ul className="gallery__ul ul">
+          {gallery
+            .filter((gallerya) => {
+              return searchByStyle.toLowerCase() === ""
+                ? gallery
+                : gallerya.style.toLowerCase().startsWith(searchByStyle);
+            })
+            .map((gallerya) => {
+              return (
+                <li className="gallery__li li" key={gallerya.id}>
+                  <div className="image-container">
+                    <Link to={`/gallery/${gallerya.id}`}>
+                      <img src={gallerya.image} alt={gallerya.alt} />
+                      <div className="overlay">
+                        <div className="text">
+                          <h3>{gallerya.title}</h3>
+                          <p>
+                            <b>Artist:</b> {gallerya.artist}
+                          </p>
+                          <p>
+                            <b>Style:</b> {gallerya.style}
+                          </p>
+                        </div>
                       </div>
-                    </div>
-                  </Link>
-                </div>
-                <div className="gallery__details">
-                  <h5>Model: {gallerya.model}</h5>
-                </div>
-              </li>
-            );
-          })}
-      </ul>
+                    </Link>
+                  </div>
+                  <div className="gallery__details">
+                    <h5>Model: {gallerya.model}</h5>
+                  </div>
+                </li>
+              );
+            })}
+        </ul>
+      </div>
     </section>
   );
 };
