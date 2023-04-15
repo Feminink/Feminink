f6ea35d782e6eb38c3acf675de3ce6b65e6711a1
Miriam Fraga Rodríguez <fraga@MacBook-Pro-de-Miriam.local>
2023-04-15 06:59:47 +0200

    import questions

---
+++ src/components/QuestionComponent/Questions.js
@@ -0,0 +1,54 @@
+const Questions = [
+    {
+      question: "¿Qué tienen en común la mayoría de mujeres tatuadoras del s.XIX y s.XX?",
+      options: [
+        {
+          response: "Empezaron sus carreras siendo tatuadas por sus parejas para ser expuestas en los freakshows de circos",
+          isCorrect: true
+        },
+        {
+          response: "No tenían las mismas oportunidades profesionales que los hombres y se veían forzadas a hacer uso de sus cuerpos para ganarse la vida",
+          isCorrect: false
+        },
+        {
+          response: "Ninguna de ellas ganaba lo mismo que los hombres por realizar el mismo trabajo ",
+          isCorrect: false
+        }
+      ]
+    },
+    {
+     question: "¿Cuál de las siguientes afirmaciones sobre pintoras del surrealismo es incorrecta?",
+      options: [
+        {
+          response: "Dorothea Tanning es la autora de 'Eine Kleine Nachtmusik' ",
+          isCorrect: false
+        },
+        {
+          response: "Remedios Varo es la autora de 'La cienca inútil o el alquimista' ",
+          isCorrect: false
+        },
+        {
+          response: "Leonora Carrington es la autora de 'El venado herido' ",
+          isCorrect: true
+        }
+      ] },
+      {
+        question: "¿A cuál de las siguientes pintoras le arrebataron la autoría de muchos de sus cuadros asignándosele esta a pintores de la corte del rey Felipe II?",
+        options: [
+          {
+         response: "Catarina van Hemessen",
+            isCorrect: false
+          },
+          {
+         response: "Artemisia Gentileschi",
+            "isCorrect": false
+          },
+          {
+         response: "Sofonisba Anguissola",
+            "isCorrect": true
+          }
+        ]
+      }
+    ]
+
+    export default Questions;
