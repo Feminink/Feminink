1cb7022c65bc59c3dd111155a43ba8b70ff75c47
Miriam Fraga Rodríguez <fraga@MacBook-Pro-de-Miriam.local>
2023-04-15 10:39:28 +0200

    not finished changes about logic in read or not

--- src/components/ProfileComponent/ProfileComponent.js
+++ src/components/ProfileComponent/ProfileComponent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import  './ProfileComponent.scss';
 import { useDispatch, useSelector } from 'react-redux';
@@ -11,15 +11,26 @@ import { Link } from 'react-router-dom';
 
 const ProfileComponent = () => {
 
-
+   const code = localStorage.getItem("_code")
    const {user} = useSelector((state)=> state.AuthReducer)
    const {messages, loadingMessages} = useSelector((state)=>state.TattooReducer);
    const dispatch = useDispatch()
    
+   const [isRead, setIsRead] = useState();
    useEffect(() => {
     dispatch(getMessages());
   }, []);
 
+  const addRead = (message) => {
+    setIsRead(prevState => {
+      const newState = {
+        ...prevState,
+        [message.id]: !prevState[message.id]
+      };
+      localStorage.setItem('isRead', JSON.stringify(newState));
+      return newState;
+    });
+  };
    let counter = [];
  
   if(loadingMessages){
@@ -33,9 +44,9 @@ return (
                  <div className='div__image__profile'> 
                    <img className='div__image__profile__image' src={user.image} alt={user.name}></img> 
                  </div>
-
                  <div  className='div__info__profile'> 
                       <h2 className='div__info__profile__h'>Welcome {user.name}</h2>
+                       <h2>{code}</h2>
                       <h3 className='div__info__profile__h'>{user.email} </h3> 
                        <ul className='ul__skills'> 
                       <h3>Tattoo skills</h3>
@@ -57,6 +68,10 @@ return (
            return(
           message.artist === user.name? 
           <div className='section__info__div__msg'> 
+          <Link onClick={() => addRead(message)} to={`/contact/${message.id}`} >
+                          <h3>Leído</h3>
+                        </Link> 
+
          <Link to={`/contact/${message.id}`}>  <h3><FontAwesomeIcon icon={faEnvelope} />  {message.name}</h3></Link>
           <h3 className='section__info__description'>{message.description}</h3>
           <h3 className='section__info__description'>Su email es {message.email}! Escríbele a {message.name} para darle cita en el estudio!</h3>
