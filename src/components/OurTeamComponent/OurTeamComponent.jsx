026e72a68bce37b94c2c6b753e5a2eacda331618
laurellx <laura.origa@gmail.com>
2023-04-16 21:28:52 +0200

    translated homepage content in back

--- src/components/OurTeamComponent/OurTeamComponent.jsx
+++ src/components/OurTeamComponent/OurTeamComponent.jsx
@@ -7,7 +7,6 @@ import { useDispatch, useSelector } from "react-redux";
 import "./OurTeamComponent.scss";
 
 const OurTeamComponent = () => {
-
   const dispatch = useDispatch();
   const { info, loadingInfo } = useSelector((state) => state.InfoReducer);
 
@@ -25,27 +24,39 @@ const OurTeamComponent = () => {
 
   return (
     <section className="section__ourteam container">
-      <h2>Meet our tattoo artist</h2>
+      <h2>Meet our tattoo artists</h2>
       <ul className="ourteam__ul ul">
-        {info && info.admins && info.admins.map((member) => {
-          return (
-            <li className="ourteam__li li" key={member.id}>
-              <div className="image-container">
-                <img src={member.image} alt={member.alt} />
-                <div className="overlay">
-                  <div className="text">
-                    <h3>{member.name} / {member.username}</h3>
-                    <p><b>Lugar: </b>{member.place}</p>
-                    <p><b>Email: </b>{member.email}</p>
+        {info &&
+          info.admins &&
+          info.admins.map((member) => {
+            return (
+              <li className="ourteam__li li" key={member.id}>
+                <div className="image-container">
+                  <img src={member.image} alt={member.alt} />
+                  <div className="overlay">
+                    <div className="text">
+                      <h3>
+                        {member.name} / {member.username}
+                      </h3>
+                      <p>
+                        <b>Where: </b>
+                        {member.place}
+                      </p>
+                      <p>
+                        <b>Email: </b>
+                        {member.email}
+                      </p>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="ourteam__details">
-                <h5>No tatuo: {member.noway[0]}, {member.noway[1]}</h5>
-              </div>
-            </li>
-          );
-        })}
+                <div className="ourteam__details">
+                  <h5>
+                    I don't tattoo: {member.noway[0]}, {member.noway[1]}
+                  </h5>
+                </div>
+              </li>
+            );
+          })}
       </ul>
     </section>
   );
