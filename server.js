295172e6b028ba5b78cfbe27ad180f7605ac7f07
laurellx <laura.origa@gmail.com>
2023-04-24 20:08:47 +0200

    fixed routes

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const port = process.env.PORT || 3000; //Line 3
 app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
 
 // create a GET route
-app.get("/express_backend", (req, res) => {
-  //Line 9
-  res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" }); //Line 10
-}); //Line 11
+app.get("/", function (req, res) {
+  res.render("index", {});
+});
+// export module
 module.exports = app;
