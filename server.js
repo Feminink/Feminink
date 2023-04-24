ba44dccb326afda1fc9e18cb93b9e538639014eb
laurellx <laura.origa@gmail.com>
2023-04-24 21:49:59 +0200

    changed json path

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
 // JSON Server module
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("./back/back.json");
+const router = jsonServer.router("back/back.json");
 
 // Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 // Add this before server.use(router)
-server.get("/", function (req, res) {
-  res.render("index", {});
-});
-
-// server.use(
-//   // Add custom route here if needed
-//   jsonServer.rewriter({
-//     "/api/*": "/$1",
-//   })
-// );
+server.use(
+  // Add custom route here if needed
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+  })
+);
 server.use(router);
 // Listen to port
 server.listen(3000, () => {
